cdf06da8a8c29bb6adb65fe6804bf8164b492a82
Mitesh <miteshtagadiya@gmail.com>
2019-07-02 12:26:08 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ed31963b22adb4f2cadab7cc7ed33ac.js"
+  "/precache-manifest.e72f60f0560de1860e899475a5f7cc88.js"
 );
 
 self.addEventListener('message', (event) => {
