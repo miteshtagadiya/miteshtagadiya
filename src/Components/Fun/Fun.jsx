ca3458d9b88bbc3e5930bf89ffd0fd7408bc5c5f
mitesh <miteshtagadiya@gmail.com>
2020-02-23 18:27:17 +0530

    Sorting visualization added

---
+++ src/Components/Fun/Fun.jsx
@@ -0,0 +1,351 @@
+import React, { Component } from "react";
+import ParticleComponent from "../ParticleComponent";
+import SelectionSort from "./SelectionSort";
+import BubbleSort from "./BubbleSort";
+
+class Fun extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4,
+      result: [],
+      test: [],
+      showCard: false,
+      score: 0,
+      highScore: 0
+    };
+  }
+
+  findCommonElement(a, b) {
+    return a.sort().toString() === b.sort().toString();
+  }
+
+  componentDidMount() {
+    this.setCode();
+  }
+
+  setCode() {
+    let array = [];
+    while (array.length < this.state.count) {
+      let random_number =
+        Math.floor(Math.random() * (this.state.count * this.state.count)) + 1;
+      if (array.indexOf(random_number) === -1) {
+        array.push(random_number);
+      }
+    }
+
+    this.setState({
+      result: array,
+      showCard: false
+    });
+    setTimeout(() => {
+      this.setState({
+        result: [],
+        test: array
+      });
+    }, 700);
+  }
+
+  //   componentDidUpdate(prevProps, prevState) {
+  //     if (this.state.result.length >= this.state.test.length) {
+  //       if (this.findCommonElement(this.state.result, this.state.test)) {
+  //         this.setState({
+  //           showMessage: true
+  //         });
+  //       }
+  //     }
+  //   }
+
+  renderBox() {
+    var arr = new Array(this.state.count).fill(this.state.count);
+    var counts = Array.from(
+      Array(this.state.count * this.state.count),
+      (x, index) => index + 1
+    );
+    return arr.map((data, index) => {
+      return (
+        <div className="row" key={index}>
+          {arr.map((val, index1) => {
+            return (
+              <div
+                onClick={() => {
+                  var joined = this.state.result.includes(
+                    counts[this.state.count * index + index1]
+                  )
+                    ? this.state.result.filter(
+                        item =>
+                          item !== counts[this.state.count * index + index1]
+                      )
+                    : this.state.result.concat(
+                        counts[this.state.count * index + index1]
+                      );
+
+                  return this.state.result.includes(
+                    counts[this.state.count * index + index1]
+                  ) === false
+                    ? this.setState({
+                        result: joined,
+                        showCard: true
+                      })
+                    : this.setState({
+                        result: joined,
+                        showCard: true
+                      });
+                }}
+                key={index1}
+                style={{
+                  borderRadius: 15,
+                  background: this.state.result.includes(
+                    counts[this.state.count * index + index1]
+                  )
+                    ? "Green"
+                    : "#FAA9F4",
+                  border: "5px solid #ff033e",
+                  minHeight: 400 / this.state.count,
+                  minWidth: 400 / this.state.count
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className="container-fluid"
+        style={{ marginTop: 70, height: "75vh", overflowY: "scroll" }}
+      >
+        <ParticleComponent />
+        <div className="row" style={{ margin: "auto" }}>
+          <div
+            className="col-sm-12 col-xl-12 col-lg-12 col-md-12"
+            style={{ margin: "auto" }}
+          >
+            <div
+              style={{
+                border: "2px solid #7ef0ff",
+                borderRadius: 10,
+                marginTop: 50
+              }}
+            >
+              <div className="row" style={{ marginTop: 30, marginBottom: 30 }}>
+                <div className="col-sm-12 col-lg-4">
+                  <label style={{ color: "aqua", padding: 20 }}>
+                    The game is just to test your short term memory, Colored
+                    boxes light up in a specific pattern. After displaying the
+                    pattern, the player must repeat the pattern by clicking the
+                    boxes in the same order. every successful completion, the
+                    pattern gets longer. If a player selects the wrong pattern
+                    the game ends.
+                  </label>
+                </div>
+                <div className="col-sm-12 col-lg-4">
+                  <div style={{ height: 400, width: 400 }}>
+                    <div>
+                      {this.state.showCard ? (
+                        this.state.result.length >= this.state.test.length &&
+                        this.state.result.length !== 0 &&
+                        this.state.test.length !== 0 ? (
+                          this.findCommonElement(
+                            this.state.result,
+                            this.state.test
+                          ) ? (
+                            <div
+                              style={{
+                                position: "absolute",
+                                display: "flex",
+                                justifyContent: "center",
+                                width: 400,
+                                marginLeft: -15,
+                                background: "black",
+                                opacity: 0.9,
+                                height: 400,
+                                flexDirection: "column",
+                                alignItems: "center"
+                              }}
+                            >
+                              <div
+                                style={{ color: "aqua", textAlign: "center" }}
+                                onClick={() => {
+                                  return this.setState(
+                                    {
+                                      result: [],
+                                      count: this.state.count + 1,
+                                      highScore:
+                                        this.state.score +
+                                          this.state.count * 10 >=
+                                        this.state.highScore
+                                          ? this.state.score +
+                                            this.state.count * 10
+                                          : this.state.highScore,
+                                      score:
+                                        this.state.score + this.state.count * 10
+                                    },
+                                    () => this.setCode()
+                                  );
+                                }}
+                              >
+                                <label style={{ fontSize: 40 }}>Great!</label>
+                                <br />
+                                <label style={{ fontSize: 30 }}>Next</label>
+                              </div>
+                            </div>
+                          ) : (
+                            <div
+                              style={{
+                                position: "absolute",
+                                display: "flex",
+                                justifyContent: "center",
+                                width: 400,
+                                marginLeft: -15,
+                                background: "black",
+                                opacity: 0.9,
+                                height: 400,
+                                flexDirection: "column",
+                                alignItems: "center"
+                              }}
+                            >
+                              <div
+                                style={{ color: "aqua", textAlign: "center" }}
+                                onClick={() => {
+                                  return this.setState(
+                                    {
+                                      result: [],
+                                      count: 4,
+                                      test: [],
+                                      showCard: false,
+                                      highScore:
+                                        this.state.score > this.state.highScore
+                                          ? this.state.score
+                                          : this.state.highScore,
+                                      score: 0
+                                    },
+                                    () => this.setCode()
+                                  );
+                                }}
+                              >
+                                <label style={{ fontSize: 30 }}>
+                                  Game Over
+                                </label>
+                              </div>
+                            </div>
+                          )
+                        ) : null
+                      ) : null}
+                      {this.renderBox()}
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-12 col-lg-4">
+                  <div
+                    style={{
+                      textAlign: "center",
+                      marginTop: 30,
+                      display: "flex",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <div
+                      style={{
+                        padding: "10px 20px",
+                        textAlign: "center",
+                        color: "aqua",
+                        width: 200,
+                        border: "2px solid",
+                        borderRadius: 20
+                      }}
+                      onClick={() => this.setCode()}
+                    >
+                      Start
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      marginTop: 30,
+                      display: "flex",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <div
+                      style={{
+                        padding: "10px 20px",
+                        fontSize: 22,
+                        textAlign: "center",
+                        color: "aqua"
+                      }}
+                    >
+                      Score
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      display: "flex",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <div
+                      style={{
+                        padding: "10px 20px",
+                        fontSize: 22,
+                        textAlign: "center",
+                        color: "aqua"
+                      }}
+                    >
+                      {this.state.score}
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      marginTop: 30,
+                      display: "flex",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <div
+                      style={{
+                        padding: "10px 20px",
+                        fontSize: 22,
+                        textAlign: "center",
+                        color: "aqua"
+                      }}
+                    >
+                      High Score
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      display: "flex",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <div
+                      style={{
+                        padding: "10px 20px",
+                        fontSize: 22,
+                        textAlign: "center",
+                        color: "aqua"
+                      }}
+                    >
+                      {this.state.highScore}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <BubbleSort />
+            <SelectionSort />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Fun;
---
+++ src/Components/Fun/SelectionSort.jsx
@@ -0,0 +1,167 @@
+import React, { Component } from "react";
+
+class SelectionSort extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addtask: 0,
+      tasks: []
+    };
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  componentDidMount() {
+    this.container = document.querySelector(".data-container1");
+  }
+
+  generateBlocks = arr => {
+    // if (num && typeof num !== "number") {
+    //   alert("First argument must be a typeof Number");
+    //   return;
+    // }
+    this.container.innerHTML = "";
+    for (let i = 0; i < arr.length; i += 1) {
+      // const value = Math.floor(Math.random() * 100);
+
+      const block = document.createElement("div");
+      block.classList.add("fun-block1");
+      block.style.height = `${arr[i] * 3}px`;
+      block.style.transform = `translateX(${i * 30}px)`;
+
+      const blockLabel = document.createElement("label");
+      blockLabel.classList.add("fun-block__id1");
+      blockLabel.innerHTML = arr[i];
+
+      block.appendChild(blockLabel);
+      this.container.appendChild(block);
+    }
+  };
+
+  swapSelectionSort = (el1, el2) => {
+    return new Promise(resolve => {
+      const style1 = window.getComputedStyle(el1);
+      const style2 = window.getComputedStyle(el2);
+
+      const transform1 = style1.getPropertyValue("transform");
+      const transform2 = style2.getPropertyValue("transform");
+
+      el1.style.transform = transform2;
+      el2.style.transform = transform1;
+
+      // Wait for the transition to end!
+      window.requestAnimationFrame(function() {
+        setTimeout(() => {
+          var temp = document.createElement("div");
+          el1.parentNode.insertBefore(temp, el1);
+
+          // move elobj1 to right before obj2
+          el2.parentNode.insertBefore(el1, el2);
+
+          // move obj2 to right before where obj1 used to be
+          temp.parentNode.insertBefore(el2, temp);
+
+          // remove temporary marker node
+          temp.parentNode.removeChild(temp);
+          // container.insertBefore(el2, el1);
+          resolve();
+        }, 250);
+      });
+    });
+  };
+
+  async selectionSort(delay = 500) {
+    if (delay && typeof delay !== "number") {
+      alert("sort: First argument must be a typeof Number");
+      return;
+    }
+    let blocks = document.querySelectorAll(".fun-block1");
+    var minIdx,
+      len = blocks.length;
+    for (let i = 0; i < blocks.length; i++) {
+      minIdx = i;
+      for (let j = i + 1; j < len; j += 1) {
+        const value1 = Number(blocks[minIdx].childNodes[0].innerHTML);
+        const value2 = Number(blocks[j].childNodes[0].innerHTML);
+
+        if (value2 < value1) {
+          minIdx = j;
+        }
+      }
+
+      if (minIdx !== i) {
+        blocks[i].style.backgroundColor = "#ff033e";
+        blocks[minIdx].style.backgroundColor = "#ff033e";
+        await new Promise(resolve =>
+          setTimeout(() => {
+            resolve();
+          }, delay)
+        );
+        await this.swapSelectionSort(blocks[minIdx], blocks[i]);
+        blocks = document.querySelectorAll(".fun-block1");
+      }
+
+      blocks[minIdx].style.backgroundColor = "#58B7FF";
+      blocks[i].style.backgroundColor = "#58B7FF";
+
+      blocks[i].style.backgroundColor = "#38e715";
+    }
+  }
+
+  updateInput(evt) {
+    this.setState({ tasks: evt.target.value.split(",") });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid" style={{ padding: 0 }}>
+        <div
+          style={{
+            border: "2px solid #7ef0ff",
+            borderRadius: 10,
+            marginTop: 50
+          }}
+        >
+          {/* <ParticleComponent /> */}
+          <label style={{ padding: 15, fontSize: 22 }}>Selection Sort</label>
+          <div className="row" style={{ justifyContent: "center" }}>
+            <input
+              type="text"
+              placeholder="23,43,12,54,23,65"
+              style={{
+                color: "#7ef0ff",
+                padding: 15,
+                borderRadius: "5px",
+                border: "2px solid #7ef0ff",
+                background: "transparent"
+              }}
+              onChange={this.updateInput}
+            />
+            <br />
+            <div
+              style={{
+                marginLeft: 15,
+                cursor: "pointer",
+                color: "#7ef0ff",
+                padding: "15px 25px",
+                borderRadius: "5px",
+                border: "2px solid #7ef0ff",
+                background: "transparent"
+              }}
+              onClick={() => {
+                this.generateBlocks(this.state.tasks);
+                this.selectionSort();
+              }}
+            >
+              Sort
+            </div>
+          </div>
+          <div style={{ padding: 25 }}>
+            <div className="data-container1" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SelectionSort;
