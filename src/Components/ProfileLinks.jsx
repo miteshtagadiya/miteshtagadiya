72733192bf0fcf22d6a7f6efc385d0916dc001ac
mitesh <miteshtagadiya@gmail.com>
2020-07-03 21:35:34 +0530

    Generate common function

--- src/Components/ProfileLinks.jsx
+++ src/Components/ProfileLinks.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "../styles/global.sass";
-import loadable from "@loadable/component";
 import battle from "../assets/battle1.png";
 import Typist from "react-typist";
 import ReactGA from "react-ga";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import windowSize from "react-window-size";
 import {
   faStackOverflow,
   faNpm,
@@ -17,6 +17,31 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 class ProfileLinks extends Component {
   render() {
+    let generateLinks = (link, name, icon) => {
+      return (
+        <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
+          <a
+            href={link}
+            style={{ textDecoration: "none", color: "#7ef0ff" }}
+            target="_blank"
+            aria-label={name}
+            rel="noopener noreferrer"
+            onClick={() => {
+              ReactGA.event({
+                category: "Social",
+                action: name + " Clicked",
+                label: name,
+              });
+            }}
+          >
+            <FontAwesomeIcon
+              icon={icon}
+              style={{ color: "#7ef0ff", marginBottom: -8 }}
+            />
+          </a>
+        </label>
+      );
+    };
     return (
       <div>
         <br />
@@ -33,133 +58,32 @@ class ProfileLinks extends Component {
           className="profile-links"
           style={{ height: 105, marginBottom: 20, position: "relative" }}
         >
-          <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
-            <a
-              href="https://github.com/miteshtagadiya"
-              style={{ textDecoration: "none", color: "#7ef0ff" }}
-              target="_blank"
-              aria-label="Github"
-              rel="noopener noreferrer"
-              onClick={() => {
-                ReactGA.event({
-                  category: "Social",
-                  action: "Github Clicked",
-                  label: "Github",
-                });
-              }}
-            >
-              <FontAwesomeIcon
-                icon={faGithub}
-                style={{ color: "#7ef0ff", marginBottom: -8 }}
-              />
-            </a>
-          </label>
-          <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
-            <a
-              href="https://gitlab.com/miteshtagadiya"
-              style={{ textDecoration: "none", color: "#7ef0ff" }}
-              target="_blank"
-              aria-label="Gitlab"
-              rel="noopener noreferrer"
-              onClick={() => {
-                ReactGA.event({
-                  category: "Social",
-                  action: "Gitlab Clicked",
-                  label: "Gitlab",
-                });
-              }}
-            >
-              <FontAwesomeIcon
-                icon={faGitlab}
-                style={{ color: "#7ef0ff", marginBottom: -8 }}
-              />
-            </a>
-          </label>
-          <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
-            <a
-              href="https://www.linkedin.com/in/mitesh-tagadiya-140420108/"
-              style={{ textDecoration: "none", color: "#7ef0ff" }}
-              target="_blank"
-              aria-label="LinkedIn"
-              rel="noopener noreferrer"
-              onClick={() => {
-                ReactGA.event({
-                  category: "Social",
-                  action: "LinkedIn Clicked",
-                  label: "LinkedIn",
-                });
-              }}
-            >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                style={{ color: "#7ef0ff", marginBottom: -8 }}
-              />
-            </a>
-          </label>
-          <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
-            <a
-              href="https://drive.google.com/file/d/1qFXjZqTDow29I2qrH1PfacNd77W9GcXa/view?usp=sharing"
-              style={{ textDecoration: "none", color: "#7ef0ff" }}
-              target="_blank"
-              aria-label="Resume"
-              rel="noopener noreferrer"
-              onClick={() => {
-                ReactGA.event({
-                  category: "Social",
-                  action: "Resume Clicked",
-                  label: "Resume",
-                });
-              }}
-            >
-              <FontAwesomeIcon
-                icon={faLink}
-                style={{ color: "#7ef0ff", marginBottom: -8 }}
-              />
-            </a>
-          </label>
-          <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
-            <a
-              href="https://stackoverflow.com/users/6880229/mitesh7172"
-              style={{ textDecoration: "none", color: "#7ef0ff" }}
-              target="_blank"
-              aria-label="StackOverflow"
-              rel="noopener noreferrer"
-              onClick={() => {
-                ReactGA.event({
-                  category: "Social",
-                  action: "StackOverflow Clicked",
-                  label: "StackOverflow",
-                });
-              }}
-            >
-              <FontAwesomeIcon
-                icon={faStackOverflow}
-                style={{ color: "#7ef0ff", marginBottom: -8 }}
-              />
-            </a>
-          </label>
-
-          <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
-            <a
-              href="https://www.npmjs.com/~miteshtagadiya"
-              style={{ textDecoration: "none", color: "#7ef0ff" }}
-              target="_blank"
-              aria-label="NPM"
-              rel="noopener noreferrer"
-              onClick={() => {
-                ReactGA.event({
-                  category: "Social",
-                  action: "Npm Clicked",
-                  label: "NPM",
-                });
-              }}
-            >
-              <FontAwesomeIcon
-                icon={faNpm}
-                style={{ color: "#7ef0ff", marginBottom: -8 }}
-              />
-            </a>
-          </label>
+          {generateLinks(
+            "https://github.com/miteshtagadiya",
+            "Github",
+            faGithub
+          )}
+          {generateLinks(
+            "https://gitlab.com/miteshtagadiya",
+            "Gitlab",
+            faGitlab
+          )}
+          {generateLinks(
+            "https://www.linkedin.com/in/mitesh-tagadiya-140420108/",
+            "LinkedIn",
+            faLinkedin
+          )}
+          {generateLinks(
+            "https://drive.google.com/file/d/1qFXjZqTDow29I2qrH1PfacNd77W9GcXa/view?usp=sharing",
+            "Resume",
+            faLink
+          )}
+          {generateLinks(
+            "https://stackoverflow.com/users/6880229/mitesh7172",
+            "StackOverflow",
+            faStackOverflow
+          )}
+          {generateLinks("https://www.npmjs.com/~miteshtagadiya", "NPM", faNpm)}
 
           <label style={{ fontSize: 28, marginLeft: 10, marginRight: 10 }}>
             <a
@@ -184,41 +108,42 @@ class ProfileLinks extends Component {
               />
             </a>
           </label>
-          <div
-            style={
-              this.props.windowWidth > 1200
-                ? {
-                    display: "auto",
-                  }
-                : { display: "none" }
+        </div>
+        <div
+          style={
+            this.props.windowWidth > 1200
+              ? {
+                  display: "auto",
+                  position: "relative"
+                }
+              : { display: "none" }
+          }
+        >
+          <a
+            href="http://voiceless-jewel.surge.sh/"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="Github"
+            onClick={() =>
+              ReactGA.event({
+                category: "GameKit View",
+                action: "GameKit View Clicked",
+                label: "GameKit View",
+              })
             }
           >
-            <a
-              href="http://voiceless-jewel.surge.sh/"
-              rel="noopener noreferrer"
-              target="_blank"
-              aria-label="Github"
-              onClick={() =>
-                ReactGA.event({
-                  category: "GameKit View",
-                  action: "GameKit View Clicked",
-                  label: "GameKit View",
-                })
-              }
+            <label
+              className="link-label-home"
+              style={{ marginBottom: 10, marginTop: 50 }}
             >
-              <label
-                className="link-label-home"
-                style={{ marginBottom: 10, marginTop: 50 }}
-              >
-                GameKit View
-                <sup style={{ padding: 15 }}>New</sup>
-              </label>
-            </a>
-          </div>
+              GameKit View
+              <sup style={{ padding: 15 }}>New</sup>
+            </label>
+          </a>
         </div>
       </div>
     );
   }
 }
 
-export default ProfileLinks;
+export default windowSize(ProfileLinks);
