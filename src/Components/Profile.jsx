3eb5e493b6e2546df36f76acb11f2f0d43468786
mitesh <miteshtagadiya@gmail.com>
2020-07-03 21:35:22 +0530

    Code refacture

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,47 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/global.sass";
 import loadable from "@loadable/component";
 import Avatar from "../assets/profile.svg";
-import "./index.sass";
 const ProfileLinks = loadable(() => import("./ProfileLinks"));
 const ParticleComponent = loadable(() => import("./ParticleComponent"));
 
-class Profile extends Component {
-  render() {
-    return (
-      <>
-        <div style={{ marginTop: 50 }}>
-          <ParticleComponent />
-          {/* <div>
-              <ButtonMenu
-                menuAlignment="right"
-                menuSize="small"
-                label="Menu"
-                icon={<FontAwesomeIcon icon={faEllipsisV} />}
-              >
-                <MenuItem label="Game kit view" />
-              </ButtonMenu>
-              <br />
-            </div> */}
-          <br />
-          <div>
-            <img
-              src={Avatar}
-              className="img-thumbnail"
-              style={{
-                borderRadius: "50%",
-                height: 230,
-                width: 230,
-              }}
-              alt="Mitesh Tagadiya"
-            />
-          </div>
+const Profile = () => {
+  return (
+    <>
+      <div style={{ marginTop: 50 }}>
+        <ParticleComponent />
+        <br />
+        <div>
+          <img
+            src={Avatar}
+            className="img-thumbnail"
+            style={{
+              borderRadius: "50%",
+              height: 230,
+              width: 230,
+            }}
+            alt="Mitesh Tagadiya"
+          />
         </div>
+      </div>
 
-        <ProfileLinks />
-      </>
-    );
-  }
-}
+      <ProfileLinks />
+    </>
+  );
+};
 
 export default Profile;
